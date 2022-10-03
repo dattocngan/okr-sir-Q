a428c1cade9de39c42a938d4edd64e6496080586
thanh-nguyen03 <nguyenducthanh240903@gmail.com>
2022-10-04 00:12:21 +0700

    add change password logic

--- src/components/Profile/EditProfile.jsx
+++ src/components/Profile/EditProfile.jsx
@@ -7,11 +7,11 @@ import {
   MDBTabs,
   MDBTabsItem,
   MDBTabsLink,
-  MDBBtn,
 } from 'mdb-react-ui-kit';
 import { editProfile, getProfile } from '../../api/http';
 import Modal from '../UI/Modal';
 import Loader from '../UI/Loader';
+import Swal from 'sweetalert2';
 
 const EditProfile = () => {
   const [userName, setUserName] = useState('');
@@ -22,8 +22,8 @@ const EditProfile = () => {
   const [password, setPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-
   const [isChangingPassword, setIsChangingPassword] = useState(false);
+
   const [isEqual, setIsEqual] = useState(false);
 
   useEffect(() => {
@@ -38,14 +38,14 @@ const EditProfile = () => {
       }
     });
   }, []);
-  
+
   useEffect(() => {
     setIsEqual(!newPassword.localeCompare(confirmPassword) ? true : false);
-  }, [newPassword, confirmPassword])
+  }, [newPassword, confirmPassword]);
 
   const submitHandle = (e) => {
     e.preventDefault();
-    
+
     if (e.target.checkValidity()) {
       if (!isChangingPassword) {
         const newProfile = {
@@ -54,15 +54,41 @@ const EditProfile = () => {
           sex: sex,
           birthday: new Date(dob),
         };
-        editProfile(newProfile).then((response) => console.log(response));
-      } 
-      else {
-        
+        setIsLoading(true);
+        editProfile(newProfile).then((response) => {
+          setIsLoading(false);
+          if (response.status === 200) {
+            localStorage.setItem('name', newProfile.name);
+            Swal.fire('Good job!', response.data.message, 'success');
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Something went wrong!',
+            });
+          }
+        });
+      } else {
         if (isEqual) {
+          setIsLoading(true);
           editProfile({
             oldPassword: password,
-            newPassword: newPassword
-          }).then(response => console.log(response));
+            newPassword: newPassword,
+          }).then((response) => {
+            setIsLoading(false);
+            if (response.status === 200) {
+              Swal.fire('Good job!', response.data.message, 'success');
+              setPassword('');
+              setNewPassword('');
+              setConfirmPassword('');
+            } else {
+              Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: response.data.message,
+              });
+            }
+          });
         }
       }
     }
@@ -93,91 +119,93 @@ const EditProfile = () => {
             </MDBTabsLink>
           </MDBTabsItem>
         </MDBTabs>
-        <MDBValidation onSubmit={submitHandle}>
-          {!isLoading && !isChangingPassword && (
-            <>
-              <MDBValidationItem className="mb-5">
-                <MDBInput label="Username" defaultValue={userName} disabled />
-              </MDBValidationItem>
-              <MDBValidationItem
-                className="mb-5"
-                feedback="Please provide your name."
-                invalid
-              >
-                <MDBInput
-                  label="Name"
-                  defaultValue={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </MDBValidationItem>
-              <MDBValidationItem
-                className="mb-5"
-                feedback="Please provide your birthday."
-                invalid
-              >
-                <MDBInput
-                  type="date"
-                  label="Birthday"
-                  defaultValue={dob}
-                  onChange={(e) => setDob(e.target.value)}
-                  required
-                />
-              </MDBValidationItem>
-              <div>
-                <MDBRadio
-                  name="Sex"
-                  id="Male"
-                  label="Male"
-                  defaultChecked={!sex ? true : false}
-                  onChange={() => setSex(0)}
-                />
-                <MDBRadio
-                  name="Sex"
-                  id="Female"
-                  label="Female"
-                  defaultChecked={sex ? true : false}
-                  onChange={() => setSex(1)}
-                />
-              </div>
-            </>
-          )}
+        {!isLoading && !isChangingPassword && (
+          <MDBValidation onSubmit={submitHandle}>
+            <MDBValidationItem className="mb-5">
+              <MDBInput label="Username" defaultValue={userName} disabled />
+            </MDBValidationItem>
+            <MDBValidationItem
+              className="mb-5"
+              feedback="Please provide your name."
+              invalid
+            >
+              <MDBInput
+                label="Name"
+                defaultValue={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </MDBValidationItem>
+            <MDBValidationItem
+              className="mb-5"
+              feedback="Please provide your birthday."
+              invalid
+            >
+              <MDBInput
+                type="date"
+                label="Birthday"
+                defaultValue={dob}
+                onChange={(e) => setDob(e.target.value)}
+                required
+              />
+            </MDBValidationItem>
+            <div>
+              <MDBRadio
+                name="Sex"
+                id="Male"
+                label="Male"
+                defaultChecked={!sex ? true : false}
+                onChange={() => setSex(0)}
+              />
+              <MDBRadio
+                name="Sex"
+                id="Female"
+                label="Female"
+                defaultChecked={sex ? true : false}
+                onChange={() => setSex(1)}
+              />
+            </div>
+            <button className="btn btn-primary mt-4">Update Profile</button>
+          </MDBValidation>
+        )}
 
-          {!isLoading && isChangingPassword && (
-            <>
-              <MDBInput
-                className="mb-4"
-                label="Old Password"
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                minLength={8}
-                required
-              />
+        {isChangingPassword && (
+          <form onSubmit={submitHandle}>
+            <MDBInput
+              className="mb-4"
+              label="Old Password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              minLength={8}
+              required
+            />
 
-              <MDBInput
-               className="mb-4"
-                label="New Password"
-                type="password"
-                onChange={(e) => setNewPassword(e.target.value)}
-                minLength={8}
-                required
-              />
-              <div className="invalid-feedback">123</div>
-              <MDBInput
-                className="mb-4"
-                label="Confirm Password"
-                type="password"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                minLength={8}
-                required
-              />
-              <span></span>
-            </>
-          )}
-          <button className="btn btn-primary mt-4">
-            {isChangingPassword ? "Change Password" : "Update Profile"}
-          </button>
-        </MDBValidation>
+            <MDBInput
+              className="mb-4"
+              label="New Password"
+              type="password"
+              onChange={(e) => setNewPassword(e.target.value)}
+              value={newPassword}
+              minLength={8}
+              required
+            />
+
+            <MDBInput
+              className=""
+              label="Confirm Password"
+              type="password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+              minLength={8}
+              required
+            />
+            <span className="d-block text-danger">
+              {!isEqual && confirmPassword && "Confirm password doesn't match"}
+            </span>
+            <button className="btn btn-primary mt-4">Change Password</button>
+          </form>
+        )}
       </div>
     </div>
   );
